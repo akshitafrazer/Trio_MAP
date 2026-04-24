9fd7795f5591cf5f489e450ab18d0702d3210627
akshitafrazer <akshitafrazer@gmail.com>
2026-04-24 15:27:16 +0530

    main

--- hii.java
+++ hii.java
@@ -1,5 +1,8 @@
 class Hi{
     public static void main(String[] args) {
          System.out.println("Hiii");
+         System.out.println("hello");
+         System.out.println("oops");
+         System.out.println("Mugguru saripoyam");
     }
 }
